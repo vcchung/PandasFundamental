8f8d0923d1206dabc9733204b88fbb7ab40c3aa9
vcchung <vcchung@gmail.com>
2020-04-20 16:55:12 +0800

    add output

--- Part1.py
+++ Part1.py
@@ -7,24 +7,55 @@ df = pd.DataFrame(data)
 # selection of column or columns
 # ---------------------------------------------------
 print(df['name'])
-print(df[['name', 'isBoy']]['isBoy'])
+# output
+# 0    peter
+# 1    Marry
+# 2     Rose
+# Name: name, dtype: object
 
-# not recommended
+print(df[['name', 'isBoy']])
+# output
+    # name  isBoy
+# 0  peter   True
+# 1  Marry  False
+# 2   Rose  False
+
+print(df[['name', 'isBoy']]['isBoy'])
+# output
+# 0     True
+# 1    False
+# 2    False
+# Name: isBoy, dtype: bool
+
+# not recommended way to select column
 # print(df.name)
 
 # Exercise 2
 # Total number of distinct isBoy
 # ---------------------------------------------------
 print(pd.unique(df['isBoy']))
-print(len(pd.unique(df['age'])))
+# output
+# [ True False]
 
+print(len(pd.unique(df['age'])))
+# output
+# 2
 
 # Exercise 3
 # Filtering
 # ---------------------------------------------------
 filtered = df['age'] == 12
 print(filtered.value_counts())
+# output
+# True     2
+# False    1
+# Name: age, dtype: int64
+
 print(df['isBoy'].value_counts())
+# output
+# False    2
+# True     1
+# Name: isBoy, dtype: int64
 
 # Exercise 4
 # Proper indexing loc and iloc
