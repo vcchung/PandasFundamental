daea72c5e13036502f7f4100cd4014ce70b11d5c
vcchung <vcchung@gmail.com>
2020-04-21 14:30:33 +0800

    add Part4

--- Part4_OutputFile.py
+++ Part4_OutputFile.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import sqlite3
 
 data = {'type': ['book', 'magazine', 'magazine', None, None, 'book', 'book', 'magazine'],
         'author': ['peter', 'peter', 'peter', 'peter', 'mary', 'mary', 'mary', 'mary'],
@@ -38,3 +39,26 @@ new_df.to_excel('data_frame.xlsx')
 
 # write file without index
 new_df.to_excel('data_frame_without_index.xlsx', index=False)
+
+# write file with filtered columns
+new_df.to_excel('data_frame_with_filtered_columns.xlsx', index=False, columns=['type', 'author'])
+
+# multiple worksheets
+excel_writer = pd.ExcelWriter('multiple_worksheets.xlsx')
+df.to_excel(excel_writer, sheet_name='original_df', index=False)
+new_df.to_excel(excel_writer, sheet_name='new_df', index=False)
+excel_writer.save()
+
+
+# Write to SQL table
+with sqlite3.connect('myDb') as conn:
+    new_df.to_sql('MyTable', conn)
+
+# Write to JSON
+print(new_df.to_json())
+# output
+# {"type":{"2":"magazine","3":null,"4":null,"5":"book"},"author":{"2":"peter","3":"peter","4":"mary","5":"mary"},"age":{"2":3,"3":4,"4":5,"5":6}}
+
+print(new_df.to_json(orient='table'))
+# output
+# {"schema":{"fields":[{"name":"index","type":"integer"},{"name":"type","type":"string"},{"name":"author","type":"string"},{"name":"age","type":"integer"}],"primaryKey":["index"],"pandas_version":"0.20.0"},"data":[{"index":2,"type":"magazine","author":"peter","age":3},{"index":3,"type":null,"author":"peter","age":4},{"index":4,"type":null,"author":"mary","age":5},{"index":5,"type":"book","author":"mary","age":6}]}
